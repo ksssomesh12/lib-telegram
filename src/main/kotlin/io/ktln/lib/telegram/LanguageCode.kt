a837cae702cddf9310453984a809eba1048fe2b2
SomesH S <ksssomesh12@gmail.com>
2021-11-06 20:17:15 +0530

    add enum classes

---
+++ src/main/kotlin/io/ktln/lib/telegram/LanguageCode.kt
@@ -0,0 +1,8 @@
+package io.ktln.lib.telegram
+
+public final enum class LanguageCode
+    (
+    public val value: String
+    )
+{
+}
---
+++ src/main/kotlin/io/ktln/lib/telegram/ParseMode.kt
@@ -0,0 +1,11 @@
+package io.ktln.lib.telegram
+
+public final enum class ParseMode
+    (
+    public val value: String
+    )
+{
+    HTML("HTML"),
+    Markdown("Markdown"),
+    MarkdownV2("MarkdownV2"),
+}
